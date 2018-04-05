4d3ec619d0d36fceb99c38e0535e16660a390782
tenoto <teruaki.enoto@gmail.com>
2018-04-05 19:36:50 +0900

    Download the list of ObsID segment table (password required)

--- scripts/get_total_gti_exposure.py
+++ scripts/get_total_gti_exposure.py
@@ -10,7 +10,19 @@ infits = sys.argv[1]
 
 total_exp = 0.0
 hdu = pyfits.open(infits)
-for line in hdu['GTI'].data:
+extname_list = []
+for i in range(len(hdu)):
+	try:
+		extname_list.append(hdu[i].header['EXTNAME'])
+	except:
+		print("no EXTNAME in the extension %d" % i)
+
+if 'GTI' in extname_list:
+	gtiname = 'GTI'
+elif 'STDGTI' in extname_list:
+	gtiname = 'STDGTI'
+
+for line in hdu[gtiname].data:
 	exp = line['STOP'] - line['START']
 	#print(line['START'],line['STOP'],exp)
 	total_exp += exp
