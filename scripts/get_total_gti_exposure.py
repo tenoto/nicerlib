b5fccc9e93d50821f36e4d0a668468682e3fc198
tenoto <teruaki.enoto@gmail.com>
2018-04-06 19:24:20 +0900

    Get total exposure of GTIs

--- scripts/get_total_gti_exposure.py
+++ scripts/get_total_gti_exposure.py
@@ -11,19 +11,30 @@ infits = sys.argv[1]
 total_exp = 0.0
 hdu = pyfits.open(infits)
 extname_list = []
+
 for i in range(len(hdu)):
 	try:
 		extname_list.append(hdu[i].header['EXTNAME'])
 	except:
-		print("no EXTNAME in the extension %d" % i)
+		pass
+		#print('skip the extension...')
 
 if 'GTI' in extname_list:
 	gtiname = 'GTI'
+	filetype = 'evt'
 elif 'STDGTI' in extname_list:
 	gtiname = 'STDGTI'
+	filetype = 'evt'	
+elif 'PREFILTER' in extname_list:
+	filetype = 'mkf'
 
-for line in hdu[gtiname].data:
-	exp = line['STOP'] - line['START']
-	#print(line['START'],line['STOP'],exp)
-	total_exp += exp
-print(total_exp)
+if filetype == 'evt':
+	for line in hdu[gtiname].data:
+		exp = line['STOP'] - line['START']
+		#print(line['START'],line['STOP'],exp)
+		total_exp += exp
+	print(total_exp)
+elif filetype == 'mkf':
+	total_exp = len(hdu['PREFILTER'].data)
+	print(total_exp)
+
