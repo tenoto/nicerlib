4c260d509165cff5b0c9d18f5c20a910a60aa742
tenoto <teruaki.enoto@gmail.com>
2018-06-15 09:02:23 +0900

    calculate luminosity

---
+++ scripts/misc/calc_luminosity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys 
+
+if len(sys.argv) != 3 :
+	sys.stderr.write('%s flux d_kpc\n' % sys.argv[0])
+	exit()
+
+flux  = float(sys.argv[1])
+d_kpc = float(sys.argv[2])
+luminosity = 1.2e+32 * (flux / 1e-12) * d_kpc**2 
+
+dump  = "flux: %.3e (erg/s/cm2)\n" % flux
+dump += "distance : %.3f (kpc)\n" % d_kpc
+dump += "luminosity: %.3e (erg/s)" % luminosity
+print(dump)
